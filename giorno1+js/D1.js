4900170ec8f64f514db52e87be2cea2cb9220641
Massimo Berlingeri <157076493+Max2002-code@users.noreply.github.com>
2024-01-29 15:31:36 +0100

    esercizio1 settimana 2

---
+++ giorno1+js/D1.js
@@ -0,0 +1,76 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+ /*String: Questo tipo di dato è una sequenza di caratteri. E' anche detto alfanumerico, Number: Questo tipo di dato è numerico. Può trattarsi di un numero intero, Boolean
+ E' il tipo di dato booleano. Può assumere solo i valori true (vero) o false (falso). Null Questo data type lo rilevo nelle variabili che ho dichiarato ma a cui non ho ancora assegnato un valore, 
+ Undefined Questo data type lo rilevo quando qualcosa non è stato ben definito in una variabile a cui ho assegnato un valore.
+ 
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+
+/*let myName='massimo';
+console.log(myName);
+
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+
+/*function somma()
+{
+    somma=12+20;
+    return somma;
+}
+console.log(somma());
+
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/*let x=12;
+console.log(x);
+
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/*let myName='massimo';
+let myName='berlingeri';
+console.log(myName);
+
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/*function sottrazione()
+{
+let x=12;
+let y=4;
+sottrazione=x-y;
+return sottrazione;
+}
+console.log(sottrazione())
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
